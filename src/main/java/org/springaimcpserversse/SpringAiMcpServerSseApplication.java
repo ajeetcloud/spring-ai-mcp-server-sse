9e0c2c72a0ea5fd9883d8507960e8de1fa75184c
Ajeet Singh <ajeet.cloud@gmail.com>
2025-06-30 17:19:59 +0530

    Added tool

--- src/main/java/org/springaimcpserversse/SpringAiMcpServerSseApplication.java
+++ src/main/java/org/springaimcpserversse/SpringAiMcpServerSseApplication.java
@@ -1,13 +1,22 @@
 package org.springaimcpserversse;
 
+import org.springaimcpserversse.service.WeatherService;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringAiMcpServerSseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAiMcpServerSseApplication.class, args);
+    }
+
+    @Bean
+    public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+        return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
     }
 
 }
